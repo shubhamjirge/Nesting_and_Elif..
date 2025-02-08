e8c787047f0df44a7ef8d91befebbfd2eee69efa
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2025-02-08 11:44:01 +0530

    Add files via upload

---
+++ Nesting_and_Elif.py
@@ -0,0 +1,17 @@
+#Nesting and Elif
+
+print("Welcome to the rollercoaster!")
+height = int(input("what is your height in cm?"))
+
+if height >= 120:
+    print("you can ride the rollercoaster!")
+    age = int(input("what is your age?"))
+    if age <= 12:
+        print("please pay $5.")
+    elif age <= 18:
+        print("please pay $7.")
+    else:
+        print("please pay $12.")
+else:
+    print("sorry you have to grow taller before you can ride!")
+
